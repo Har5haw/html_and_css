8986c9a1f927e905d968fac04b3cb38e9eed5cdd
Har5haw <harshavardhan.gadireddy@zemosolabs.com>
2021-04-30 08:57:28 +0530

    validation bug and ui impovements

--- scripts/script.js
+++ scripts/script.js
@@ -1,41 +1,4 @@
-var timer;
 
-let email_value = "";
-let message_value = "";
-
-function validateEmail(email) {
-    if (email || email_value) {
-        email_value = email;
-        clearTimeout(timer);
-        timer = setTimeout(emailTypingDone, 300);
-    }
-}
-
-function emailTypingDone() {
-    let input = document.getElementById("email-input");
-    if (!email_value.match("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$")) {
-        input.className = "inputError";
-    } else {
-        input.className = "inputNoError";
-    }
-}
-
-function validateMessage(message) {
-    if (message || message_value) {
-        message_value = message;
-        clearTimeout(timer);
-        timer = setTimeout(messageTypingDone, 300);
-    }
-}
-
-function messageTypingDone() {
-    let input = document.getElementById("message-input");
-    if (message_value.length < 10) {
-        input.className = "inputError";
-    } else {
-        input.className = "inputNoError";
-    }
-}
 
 function getImgPromise(url) {
     const img = new Image();
